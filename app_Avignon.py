3f11ebf2cebfcd2ca27783c1bebb625c905c83df
jnicoloso-91 <joel.nicoloso@gmail.com>
2025-07-17 11:17:04 +0200

    Update app_Avignon.py
    Signed-off-by: jnicoloso-91 <joel.nicoloso@gmail.com>

--- app_Avignon.py
+++ app_Avignon.py
@@ -34,7 +34,7 @@ def afficher_titre():
         """
         <style>
             .block-container {
-                padding-top: 1rem;
+                padding-top: 2rem;
             }
         </style>
         """, unsafe_allow_html=True
