a04454b52967e8f133ff5604c50225dd3af45856
jnicoloso-91 <joel.nicoloso@gmail.com>
2025-07-20 20:05:10 +0200

    Update app_Avignon.py

--- app_Avignon.py
+++ app_Avignon.py
@@ -773,7 +773,7 @@ def afficher_activites_planifiees(df):
                             valeur_actuelle = row[colonne]
                         else:
                             valeur_actuelle = lien
-                        if pd.notna(valeur_actuelle):
+                        if not pd.notna(valeur_actuelle):
                             valeur_actuelle = ""
                         nouvelle_valeur = st.text_input(f"✏️ Edition", valeur_actuelle) 
                         submitted = st.button("✅ Valider", key="validation_editeur_activites_planifiees")
@@ -967,7 +967,7 @@ def afficher_activites_non_planifiees(df):
                             valeur_actuelle = row[colonne]
                         else:
                             valeur_actuelle = lien
-                        if pd.notna(valeur_actuelle):
+                        if not pd.notna(valeur_actuelle):
                             valeur_actuelle = ""
                         nouvelle_valeur = st.text_input(f"✏️ Edition", valeur_actuelle)
                         submitted = st.button("✅ Valider", key="validation_editeur_activites_non_planifiees")
