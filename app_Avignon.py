a06f1780309fcc938ca4abb925a17f42a11c31a2
jnicoloso-91 <joel.nicoloso@gmail.com>
2025-09-09 00:14:35 +0200

    Update app_Avignon.py
    Signed-off-by: jnicoloso-91 <joel.nicoloso@gmail.com>

--- app_Avignon.py
+++ app_Avignon.py
@@ -5093,7 +5093,7 @@ def main():
 
     # Affichage du titre
     # debug_trace("afficher_titre", trace_type=["gen"])
-    afficher_titre("Planificateur Avignon Of")
+    afficher_titre("Planificateur Avignon Off")
 
     # version = pkg_resources.get_distribution("streamlit-aggrid").version
     # st.write("Version streamlit-aggrid :", version)
